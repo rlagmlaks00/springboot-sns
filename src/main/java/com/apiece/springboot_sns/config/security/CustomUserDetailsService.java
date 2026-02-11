a762fe8118667ca9ba46a5f97d89cb53295ea425
funimani <gmlaksgmlaks@naver.com>
2026-02-11 17:13:30 +0900

    refactor: config 패키지 분리 및 도메인 엔티티 롬복 적용

---
+++ src/main/java/com/apiece/springboot_sns/config/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.apiece.springboot_sns.config.security;
+
+import com.apiece.springboot_sns.domain.user.User;
+import com.apiece.springboot_sns.domain.user.UserService;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username);
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}
